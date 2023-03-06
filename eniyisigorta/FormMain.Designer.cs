41e206f29c1597bd7cd8171a5a6b11dc0b0ad86b
eminarif <coderwax@gmail.com>
2023-03-07 01:42:22 +0300

    Update

--- eniyisigorta/FormMain.Designer.cs
+++ eniyisigorta/FormMain.Designer.cs
@@ -33,18 +33,12 @@
             this.pnlContent = new System.Windows.Forms.Panel();
             this.pnlSidebar = new System.Windows.Forms.Panel();
             this.pnlMenu = new System.Windows.Forms.Panel();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.lblCopyright = new System.Windows.Forms.Label();
             this.btnPotential = new System.Windows.Forms.Button();
             this.btnTransfer = new System.Windows.Forms.Button();
             this.btnOffer = new System.Windows.Forms.Button();
             this.btnHome = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.lblCopyright = new System.Windows.Forms.Label();
             this.pnlContainer.SuspendLayout();
             this.pnlSidebar.SuspendLayout();
             this.pnlMenu.SuspendLayout();
@@ -53,19 +47,20 @@
             // 
             // pnlContainer
             // 
-            this.pnlContainer.Controls.Add(this.pnlContent);
             this.pnlContainer.Controls.Add(this.pnlSidebar);
-            this.pnlContainer.Location = new System.Drawing.Point(0, 64);
+            this.pnlContainer.Location = new System.Drawing.Point(0, 63);
+            this.pnlContainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlContainer.Name = "pnlContainer";
-            this.pnlContainer.Size = new System.Drawing.Size(1369, 837);
+            this.pnlContainer.Size = new System.Drawing.Size(1200, 1030);
             this.pnlContainer.TabIndex = 0;
             // 
             // pnlContent
             // 
             this.pnlContent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
-            this.pnlContent.Location = new System.Drawing.Point(256, 0);
+            this.pnlContent.Location = new System.Drawing.Point(298, 63);
+            this.pnlContent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlContent.Name = "pnlContent";
-            this.pnlContent.Size = new System.Drawing.Size(1113, 837);
+            this.pnlContent.Size = new System.Drawing.Size(902, 639);
             this.pnlContent.TabIndex = 2;
             // 
             // pnlSidebar
@@ -75,136 +70,35 @@
             this.pnlSidebar.Controls.Add(this.pictureBox1);
             this.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlSidebar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSidebar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlSidebar.Name = "pnlSidebar";
-            this.pnlSidebar.Size = new System.Drawing.Size(256, 837);
+            this.pnlSidebar.Size = new System.Drawing.Size(300, 1030);
             this.pnlSidebar.TabIndex = 1;
             // 
             // pnlMenu
             // 
             this.pnlMenu.BackColor = System.Drawing.Color.White;
-            this.pnlMenu.Controls.Add(this.button7);
-            this.pnlMenu.Controls.Add(this.button2);
-            this.pnlMenu.Controls.Add(this.button1);
             this.pnlMenu.Controls.Add(this.lblCopyright);
-            this.pnlMenu.Controls.Add(this.button6);
-            this.pnlMenu.Controls.Add(this.button5);
-            this.pnlMenu.Controls.Add(this.button4);
             this.pnlMenu.Controls.Add(this.btnPotential);
             this.pnlMenu.Controls.Add(this.btnTransfer);
             this.pnlMenu.Controls.Add(this.btnOffer);
             this.pnlMenu.Controls.Add(this.btnHome);
-            this.pnlMenu.Location = new System.Drawing.Point(0, 109);
+            this.pnlMenu.Location = new System.Drawing.Point(0, 145);
+            this.pnlMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlMenu.Name = "pnlMenu";
-            this.pnlMenu.Size = new System.Drawing.Size(256, 728);
+            this.pnlMenu.Size = new System.Drawing.Size(341, 494);
             this.pnlMenu.TabIndex = 3;
             // 
-            // button7
+            // lblCopyright
             // 
-            this.button7.BackColor = System.Drawing.Color.White;
-            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button7.FlatAppearance.BorderSize = 0;
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold);
-            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button7.Image = global::eniyisigorta.Properties.Resources.home;
-            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button7.Location = new System.Drawing.Point(0, 618);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(256, 60);
-            this.button7.TabIndex = 11;
-            this.button7.Text = "                Test 8";
-            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button7.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold);
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Image = global::eniyisigorta.Properties.Resources.home;
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(0, 552);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(256, 60);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "                Test 7";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold);
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Image = global::eniyisigorta.Properties.Resources.home;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(0, 486);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(256, 60);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "                Test 6";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.Color.White;
-            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold);
-            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button6.Image = global::eniyisigorta.Properties.Resources.home;
-            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button6.Location = new System.Drawing.Point(0, 420);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(256, 60);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "                Test 4";
-            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button6.UseVisualStyleBackColor = false;
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.White;
-            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold);
-            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button5.Image = global::eniyisigorta.Properties.Resources.home;
-            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button5.Location = new System.Drawing.Point(0, 350);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(256, 60);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "                Test 3";
-            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button5.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.White;
-            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold);
-            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button4.Image = global::eniyisigorta.Properties.Resources.home;
-            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.Location = new System.Drawing.Point(0, 280);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(256, 60);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "                Test 2";
-            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.UseVisualStyleBackColor = false;
+            this.lblCopyright.AutoSize = true;
+            this.lblCopyright.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCopyright.Location = new System.Drawing.Point(16, 864);
+            this.lblCopyright.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(207, 19);
+            this.lblCopyright.TabIndex = 8;
+            this.lblCopyright.Text = "© Copyright 2020 eniyisigorta";
             // 
             // btnPotential
             // 
@@ -216,9 +110,10 @@
             this.btnPotential.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnPotential.Image = global::eniyisigorta.Properties.Resources.potential;
             this.btnPotential.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPotential.Location = new System.Drawing.Point(0, 210);
+            this.btnPotential.Location = new System.Drawing.Point(0, 258);
+            this.btnPotential.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPotential.Name = "btnPotential";
-            this.btnPotential.Size = new System.Drawing.Size(256, 60);
+            this.btnPotential.Size = new System.Drawing.Size(300, 74);
             this.btnPotential.TabIndex = 3;
             this.btnPotential.Text = "                Potansiyel";
             this.btnPotential.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -235,9 +130,10 @@
             this.btnTransfer.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnTransfer.Image = global::eniyisigorta.Properties.Resources.transfer;
             this.btnTransfer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnTransfer.Location = new System.Drawing.Point(0, 140);
+            this.btnTransfer.Location = new System.Drawing.Point(0, 172);
+            this.btnTransfer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnTransfer.Name = "btnTransfer";
-            this.btnTransfer.Size = new System.Drawing.Size(256, 60);
+            this.btnTransfer.Size = new System.Drawing.Size(300, 74);
             this.btnTransfer.TabIndex = 2;
             this.btnTransfer.Text = "                Transfer";
             this.btnTransfer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -254,9 +150,10 @@
             this.btnOffer.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnOffer.Image = ((System.Drawing.Image)(resources.GetObject("btnOffer.Image")));
             this.btnOffer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnOffer.Location = new System.Drawing.Point(0, 70);
+            this.btnOffer.Location = new System.Drawing.Point(0, 86);
+            this.btnOffer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOffer.Name = "btnOffer";
-            this.btnOffer.Size = new System.Drawing.Size(256, 60);
+            this.btnOffer.Size = new System.Drawing.Size(300, 74);
             this.btnOffer.TabIndex = 1;
             this.btnOffer.Text = "                Teklif Al";
             this.btnOffer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -274,8 +171,9 @@
             this.btnHome.Image = global::eniyisigorta.Properties.Resources.home;
             this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnHome.Location = new System.Drawing.Point(0, 0);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(256, 60);
+            this.btnHome.Size = new System.Drawing.Size(300, 74);
             this.btnHome.TabIndex = 0;
             this.btnHome.Text = "                Anasayfa";
             this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -285,34 +183,27 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::eniyisigorta.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(38, 19);
+            this.pictureBox1.Location = new System.Drawing.Point(32, 23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(180, 68);
+            this.pictureBox1.Size = new System.Drawing.Size(240, 84);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // lblCopyright
-            // 
-            this.lblCopyright.AutoSize = true;
-            this.lblCopyright.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblCopyright.Location = new System.Drawing.Point(12, 702);
-            this.lblCopyright.Name = "lblCopyright";
-            this.lblCopyright.Size = new System.Drawing.Size(169, 15);
-            this.lblCopyright.TabIndex = 8;
-            this.lblCopyright.Text = "© Copyright 2020 eniyisigorta";
-            // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1368, 900);
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Controls.Add(this.pnlContent);
             this.Controls.Add(this.pnlContainer);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1368, 900);
-            this.MinimumSize = new System.Drawing.Size(1368, 900);
+            this.MaximumSize = new System.Drawing.Size(1200, 700);
+            this.MinimumSize = new System.Drawing.Size(1200, 700);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "eniyisigorta";
@@ -335,15 +226,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel pnlMenu;
         private System.Windows.Forms.Button btnHome;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button btnPotential;
         private System.Windows.Forms.Button btnTransfer;
         private System.Windows.Forms.Button btnOffer;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lblCopyright;
     }
 }
